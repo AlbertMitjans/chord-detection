b609881c87ce35f73a35252fd92bc1987a067bc3
mitjans <a.mitjanscoma@gmail.com>
2020-05-11 17:25:23 -0600

    get weights

--- checkpoints/get_weights.sh
+++ checkpoints/get_weights.sh
@@ -1,5 +1,9 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1n7Cwc7UWHaZ88vGpPoLlX_yJnAu1L6TT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1n7Cwc7UWHaZ88vGpPoLlX_yJnAu1L6TT" -O ckpts.zip && rm -rf /tmp/cookies.txt
 
+mkdir best_ckpt
+
+mv ckpts.zip best_ckpt/ckpts.zip
+
 unzip ckpts.zip
 
 rm ckpts.zip
