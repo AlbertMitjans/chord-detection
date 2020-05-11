4970a2454ac1bfa019f9d089e93eb3eb75d63a9a
mitjans <a.mitjanscoma@gmail.com>
2020-05-11 16:44:58 -0600

    gitignore

--- checkpoints/get_weights.sh
+++ checkpoints/get_weights.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1NV5uaysA6x8rge6A2SgmaU1OrgDhrdn6' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1NV5uaysA6x8rge6A2SgmaU1OrgDhrdn6" -O ckpts.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1n7Cwc7UWHaZ88vGpPoLlX_yJnAu1L6TT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1n7Cwc7UWHaZ88vGpPoLlX_yJnAu1L6TT" -O ckpts.zip && rm -rf /tmp/cookies.txt
 
 unzip ckpts.zip
 
