e4b0c0026b87f33b258aa9257c35147d0c4b0dc5
Albert Mitjans <a.mitjanscoma@gmail.com>
2020-11-30 22:49:19 +0100

    Update get_dataset.sh

--- data/get_dataset.sh
+++ data/get_dataset.sh
@@ -1,5 +1,4 @@
 gdown https://drive.google.com/uc?id=1Bh_MMfSJm1BqqxL7bZyO4bSREODSHZYL
 unzip UCCS-UB-guitar-dataset.zip
 mv UCCS-UB-guitar-dataset/* .
-rm -r UCCS-UB-guitar-dataset
-rm UCCS-UB-guitar-dataset.zip
+rm -r UCCS-UB-guitar-dataset UCCS-UB-guitar-dataset.zip
